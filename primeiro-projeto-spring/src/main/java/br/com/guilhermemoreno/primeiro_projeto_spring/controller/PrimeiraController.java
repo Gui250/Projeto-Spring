36dd1c57ddaebfac3a3f4b880a71287ba0c27582
Guilherme Moreno <guilhermemoreno200@gmail.com>
2026-08-16 18:12:24 -0300

    alterações feitas

--- primeiro-projeto-spring/src/main/java/br/com/guilhermemoreno/primeiro_projeto_spring/controller/PrimeiraController.java
+++ primeiro-projeto-spring/src/main/java/br/com/guilhermemoreno/primeiro_projeto_spring/controller/PrimeiraController.java
@@ -2,8 +2,11 @@ package br.com.guilhermemoreno.primeiro_projeto_spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 //Anotação que indica que a classe é um controlador REST
 @RestController
@@ -14,5 +17,15 @@ public class PrimeiraController {
     @GetMapping("/primeiroMetodo/{id}")
     public String primeiraMensagem(@PathVariable(name = "id") String id){
         return "Olá Mundo " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam(name = "id") String id){
+        return "Olá Mundo " + id;
+    }
+
+    @PostMapping("/metodoComBody")
+    public String metodoComBody(@RequestBody String username){
+        return "Olá Mundo " + username;
     }
 }
